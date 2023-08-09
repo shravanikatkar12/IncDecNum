b047fa5eb594bdcf090f20de0e00068d44bff666
Shravani Katkar <118427244+shravanikatkar12@users.noreply.github.com>
2023-08-09 11:15:58 +0530

    index.js file added

---
+++ ToDoList.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+
+
+const ToDoList=()=>{
+  const [num, setNum] = useState(0);
+  const incNum=()=>{
+    setNum(num+1);
+  };
+  const decNum=()=>{
+    if(num>0){
+    setNum(num-1);
+    }
+    else{
+      alert("sorry, zero limit");
+      setNum(0);
+    }
+  };
+   return(
+    <>
+      <div className='main_div'>
+        <div className='center_div'>
+        <h1>{num}</h1>
+          <div className='btn_div'>
+            <button onClick={incNum}>Increm</button>
+            <button onClick={decNum}>Decrem</button>
+          </div>
+        </div>
+      </div>
+    </>
+   );
+};
+
+
+
+
+export default ToDoList;
---
+++ index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ToDoList from './ToDoList';
+import './index.css'
+
+
+
+
+
+ReactDOM.render(<ToDoList/>,
+    document.getElementById("root")
+);
+
+
